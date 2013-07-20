29077b263b35c71e0c8cab6bba819636b4be9779
Ubuntu <ubuntu@ip-172-31-35-16.us-west-2.compute.internal>
2013-07-20 22:46:30 +0000

    grader

--- web.js
+++ web.js
@@ -19,7 +19,7 @@ app.get('/', function(request, response) {
  
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
+var port = process.env.PORT || 5000
+  app.listen(port, function() {
   console.log("Listening on " + port);
 });
