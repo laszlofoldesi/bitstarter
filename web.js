a88e9f3ef67c949da8e4437b614bf64ffcb24f5b
laszlofoldesi <laszlofoldesi@me.com>
2013-07-22 19:36:11 +0000

    Stor3.com is alive

--- web.js
+++ web.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-
+var htmlfile = 'index.html';
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- // var content;
- //    fs.readFile('./index.html', function read(err, data) {
-//	if (err) {
-  //          throw err;
-//	}
-//	content = data.toString('utf-8');
-//	response.send(data);
-//    });
 
-    var buffer = fs.readFileSync("index.html");
-
-    response.send(buffer.toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 8080;
   app.listen(port, function() {
   console.log("Listening on " + port);
 });
